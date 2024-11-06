48e36b79da03c6524318c68f5d0481195a5396b1
kimywann <kimywan10@gmail.com>
2024-11-06 22:52:11 +0900

    feat(OutputView): 영수증 출력 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -6,13 +6,20 @@ const OutputView = {
         StoreList.products.forEach((product) => {
             const { name, price, quantity, promotion } = product;
             const output = `- ${name} ${price.toLocaleString()}원 ${quantity}개 ${promotion}`;
-            
+
             // TODO: 재고 없음 0 출력 
             Console.print(output);
         // ...
         })
-    // ...
+    },
+
+    printReceipt(products) {
+        Console.print("==============W 편의점================");
+        Console.print("상품명\t\t수량\t\t금액");
+
+        products.forEach((product) => {
+            Console.print(`${product.name}\t\t${product.quantity}\t\t${product.totalPrice}`);
+        });
     }
 }
-
 export default OutputView;

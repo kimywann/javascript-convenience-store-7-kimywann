051cab07a7a207dd44bc01d4bf2fbfe6212e08ff
kimywann <kimywan10@gmail.com>
2024-11-06 13:15:29 +0900

    feat(OutputView): 상품 목록 출력 기능 구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+import StoreList from '../storelist.js';
+
+const OutputView = {
+    printProducts() {
+        StoreList.products.forEach((product) => {
+            const { name, price, quantity, promotion } = product;
+            const output = `- ${name} ${price.toLocaleString()}원 ${quantity}개 ${promotion}`;
+            
+            // TODO: 재고 없음 0 출력 
+            Console.print(output);
+        // ...
+        })
+    // ...
+    }
+}
+
+export default OutputView;

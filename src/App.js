049e4406a13c46c152c5b7db53b62e90df760e49
kimywann <Kimywan10@gmail.com>
2024-11-06 23:29:07 +0900

    feat(App): run 메서드를 통해 상품 목록 출력, 구매 입력, 영수증 출력 흐름 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "./view/InputView.js";
 import OutputView from "./view/OutputView.js";
 
 class App {
   async run() {
-    OutputView.printProducts();
+    Console.print("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n")
+    OutputView.printItemList();
+    const purchaseItems = await InputView.readItem(); 
+    OutputView.printReceipt(purchaseItems);
   }
 }
 

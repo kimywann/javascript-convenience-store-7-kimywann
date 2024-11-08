c13fc9a1effda3d1e25de162368d7de888327e5f
kimywann <Kimywan10@gmail.com>
2024-11-08 21:48:36 +0900

    feat(OutputView): 재고 상태 및 프로모션 정보를 조건에 따라 표시하는 기능 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,16 +1,31 @@
 import { Console } from '@woowacourse/mission-utils';
-import Store from '../Store.js';
+import Store from '../Controller/Store.js';
 
 const OutputView = {
     printItemList() {
         Store.itemList.forEach((product) => {
             const { name, price, quantity, promotion } = product;
-            const output = `- ${name} ${price.toLocaleString()}원 ${quantity}개 ${promotion}`;
 
-            // TODO: 재고 없음 0 출력 
+            let quantityText = '';
+            if (quantity === '재고 없음' || quantity === 0) {
+                quantityText = '재고 없음';
+            } else {
+                quantityText = `${quantity}개`;
+            }
+
+            let promotionText = '';
+            if (promotion !== null) {
+                promotionText = promotion;
+            }
+
+            if (quantityText === '재고 없음') {
+                promotionText = '';
+            }
+
+            // 출력 형식
+            const output = `- ${name} ${price.toLocaleString()}원 ${quantityText} ${promotionText}`;
             Console.print(output);
-        // ...
-        })
+        });
     },
 
     printReceipt(products) {

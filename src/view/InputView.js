5dd129c5c1051a54a0cae36ef263bf6a7c8a2799
kimywann <Kimywan10@gmail.com>
2024-11-11 00:02:41 +0900

    chore(InputView): Store 모듈 경로를 Controller 폴더로 이동하여 파일 구조 정리

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,19 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
-import Store from '../Store.js';
+import Store from '../Controller/Store.js';
 
 const InputView = {
     async readItem() {
         const input = await Console.readLineAsync("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n");
         const productArray = input.replace(/\[|\]/g, "").split(",");
 
-        // 상품 목록과 총 금액 계산을 Store 모델에서 처리
         const printItems = Store.getProductsWithTotalPrice(productArray);
         return printItems;
+    },
+
+    async readMemberShip() {
+            const memberShipInput = await Console.readLineAsync("멤버십 할인을 받으시겠습니까? (Y/N)\n");
+            return memberShipInput
     }
-    // TODO: 멤버십 할인 
-    // async readMemberShip() {
-    //         const memberShipInput = await Console.readLineAsync("멤버십 할인을 받으시겠습니까? (Y/N)\n");
-    //         return memberShipInput
-    // }
 }
 export default InputView;

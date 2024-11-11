4bcad7f2e4e2ad28e655f3eacddea66c120bd1aa
kimywann <Kimywan10@gmail.com>
2024-11-11 14:36:38 +0900

    feat(OutputView): 구매 내역 및 증정 상품 출력 기능 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -28,13 +28,32 @@ const OutputView = {
         });
     },
 
-    printReceipt(products) {
-        Console.print("==============W 편의점================");
-        Console.print("상품명\t\t수량\t\t금액");
+    printReceipt(products, totalAmount, promotionDiscount, membershipDiscount, finalAmount, totalQuantity) {
+    Console.print("==============W 편의점================");
+    Console.print("상품명\t\t수량\t\t금액");
 
-        products.forEach((product) => {
-            Console.print(`${product.name}\t\t${product.quantity}\t\t${product.totalPrice}`);
-        });
-    }
+    products.forEach((product) => {
+        Console.print(`${product.name}\t\t${product.quantity}\t\t${product.totalPrice}`);
+    });
+
+    Console.print("=====================================");
+    Console.print(`총구매액\t${totalQuantity}\t${totalAmount.toLocaleString()}원`);
+    Console.print(`행사할인\t\t\t-${promotionDiscount.toLocaleString()}원`);
+
+    const membershipDiscountText = membershipDiscount === 'Y' ? `-${(0.05 * totalAmount).toLocaleString()}원` : '-0원';
+    Console.print(`멤버십할인\t\t\t${membershipDiscountText}`);
+    Console.print(`내실돈\t\t\t${finalAmount.toLocaleString()}원`);
+    },
+
+    printGiftItems(products) {
+        const giftItems = Store.calculateGiftItems(products.map(product => `${product.name}-${product.quantity}`));
+        
+        if (giftItems.length > 0) {
+            Console.print("==============증     정===============");
+            giftItems.forEach((gift) => {
+            Console.print(`${gift.name}\t\t${gift.quantity}`);
+            });
+        }
+    },
 }
 export default OutputView;
